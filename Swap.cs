9453df1488b6a22f1e0d50301ba4825c7c24d317
Shashank <shashankpateriya4@gmail.com>
2021-07-09 21:19:43 +0530

    [shashank] Add: Swapping of 2 numbers

---
+++ Swap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    public class Swap
+    {
+        public void SwapNumber()
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
