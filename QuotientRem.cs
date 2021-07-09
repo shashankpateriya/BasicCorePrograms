10269c5d99423ea40d5afb873796b5fc36f7fb46
Shashank <shashankpateriya4@gmail.com>
2021-07-09 21:18:38 +0530

    [shashank] Add: Calculating quotient and remainder

---
+++ QuotientRem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    public class QuotientRem
+    {
+        public void QuotientRemainder()
+        {
+            int dividend = 100, divisor = 5;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
