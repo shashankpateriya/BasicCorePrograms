b91bf0aeb3c6dbd4de4e095101f89c8101ec823f
Shashank <shashankpateriya4@gmail.com>
2021-07-09 21:15:04 +0530

    [shashank] Add: Harmonic number and its sum

---
+++ HarmonicNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    public class HarmonicNumber
+    {
+        public void ReadInput()
+        {
+            Console.WriteLine("Enter the nth harmonic number");
+            int hormonic = Convert.ToInt32(Console.ReadLine());
+            HarmonicNumber harmonicNumber = new HarmonicNumber();
+            harmonicNumber.FindHarmonicNumber(hormonic);
+        }
+        private void FindHarmonicNumber(int harmonic)
+        {
+            double sum = 0;
+            for (int i = 1; i <= harmonic; i++)
+            {
+                sum = sum + (double)1 / i;
+            }
+            Console.WriteLine(harmonic + "th harmonic value = " + sum);
+        }
+    }
+}
