0793ac7b33378c371ec16868a63e94b78bc008ae
Shashank <shashankpateriya4@gmail.com>
2021-07-09 21:20:56 +0530

    [shashank] Add: Check number is even or odd

---
+++ EvenOrOdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    public class EvenOrOdd
+    {
+        public void EvenOdd()
+        {
+            int i;
+            Console.WriteLine("enter the number");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+                Console.WriteLine("Even Number");
+            else
+                Console.WriteLine("Odd Number");
+        }
+    }
+}
