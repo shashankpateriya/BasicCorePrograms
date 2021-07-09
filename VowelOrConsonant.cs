90295b123a7f0a86deac1d988b2008ffeedfc8aa
Shashank <shashankpateriya4@gmail.com>
2021-07-09 21:22:32 +0530

    [shashank] Add: Check alphabet is vowel or consonant

---
+++ VowelOrConsonant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    public class VowelOrConsonant
+    {
+        public void VowelConst()
+        {
+            char ch;
+
+            Console.WriteLine("Enter any Alphabet: ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if ((ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'))
+            {
+                Console.WriteLine(" is Vowel");
+            }
+            else
+            {
+                Console.WriteLine(" is Consonant");
+                //  Console.ReadLine();
+            }
+        }
+    }
+}
